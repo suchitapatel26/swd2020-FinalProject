e5d4fab174aac1d06f1dd004153c7de7119c9963
Suchita Patel <spatel35@uiowa.edu>
2021-09-12 19:09:10 -0500

    Create SQLConnectionInterface.java

---
+++ FantasyGame/src/com/team07/SQL/SQLConnectionInterface.java
@@ -0,0 +1,15 @@
+package com.team07.SQL;
+
+/**
+ * Interface SQLConnectionInterface
+ */
+public interface SQLConnectionInterface {
+
+    public final static String MySQLURL = "jdbc:mysql://s-l112.engr.uiowa.edu:3306/engr_class007";      // mySQL URL
+
+    public final static String DB_PASS = "Database connection is successful!!";                         // Message if successful connected
+
+
+
+
+}
