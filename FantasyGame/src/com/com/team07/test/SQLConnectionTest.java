f0294a4d6623a9ec885e8d1363a51b3e8b618886
Suchita Patel <spatel35@uiowa.edu>
2021-09-12 19:08:32 -0500

    Create SQLConnectionTest.java

---
+++ FantasyGame/src/com/com/team07/test/SQLConnectionTest.java
@@ -0,0 +1,20 @@
+package com.com.team07.test;
+
+import com.team007.model.Match;
+import com.team007.model.Player;
+import com.team07.SQL.SQLConnectionImplement;
+import com.team07.SQL.SQLFunctions;
+
+import java.awt.*;
+import java.lang.management.PlatformLoggingMXBean;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * Class SQLConnectionTest to run functionality of SQLConnectionImplement
+ */
+public class SQLConnectionTest {
+    public static void main(String[] args ){
+        SQLFunctions function = new SQLFunctions();
+    }
+
+}
